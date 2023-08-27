23883cf57a2c0a7422254446a1fbba3937ba5cdb
LONEWOLF143 <twaha2030@gmail.com>
2023-08-27 15:09:42 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <h1 className="mb-6 mt-6 px-6 py-6 text-center text-3xl underline md:text-4xl">
+      Hello world!
+    </h1>
+  );
+};
+
+export default App;
