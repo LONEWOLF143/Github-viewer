9cb3ca18a6a2d31386e9c40db52d082e822cf288
LONEWOLF143 <twaha2030@gmail.com>
2023-09-04 19:50:05 +0300

    modified gist list added file names

--- src/pages/Gists.tsx
+++ src/pages/Gists.tsx
@@ -4,8 +4,16 @@ import {FC, useContext} from 'react'
 const Gists:FC = () => {
   const {gistsList} = useContext(GitContext)
   return (
+    
     <div>{gistsList?.map(gist => (
-      <h1>{gist.html_url}</h1>
+      <div>
+      <h1><a href={gist.html_url} target="_blank" rel='noreferrence'>{gist.html_url}</a></h1>
+      <p>{gist.description}</p>
+      {/* getting all file names for each gist, used Object.keys so that react does not give us warning about missing keys*/}
+      {Object.keys(gist.files).map(filename => (
+            <p key={filename}>{gist.files[filename].filename}</p>
+          ))}
+      </div>
     ))}</div>
   )
 }
--- src/pages/Repos.tsx
+++ src/pages/Repos.tsx
@@ -30,14 +30,14 @@ const Repos: FC = () => {
   return (
     <div className="mx-auto flex max-w-4xl flex-wrap gap-2">
       {repositoriesList?.map((repos) => (
-        <a href={repos.html_url} target="_blank" rel="noreferrer">
+        <a href={repos.html_url} target="_blank" rel="noreferrer " className='mx-auto'>
           <div className="mx-auto flex rounded h-[100px] w-[400px] flex-col justify-between bg-[#98E1AD] px-3 py-3">
           <a href={repos.html_url} target="_blank" rel="noreferrer" className='text-[1rem] tracking-[0.2rem] font-[600]'>
             {repos.name}
           </a>
-          <div className="mt-3 flex justify-between">
-            <p className="mr-4 text-[0.75rem] tracking-[0.075rem] font-[400]">Created: {extractDate(repos.created_at)}</p>
-            <p className='text-[0.75rem] tracking-[0.075rem] font-[400]'> Last updated: {findLatestUpdateDate(repos)}</p>
+          <div className="mt-3 flex justify-between  ">
+            <p className="mr-4 text-[0.55rem] sm:text-[0.75rem] tracking-[0.075rem] font-[400]">Created: {extractDate(repos.created_at)}</p>
+            <p className='text-[0.55rem] sm:text-[0.75rem] tracking-[0.075rem] font-[400]'> Last updated: {findLatestUpdateDate(repos)}</p>
           </div>
         </div>
         </a>
